7d833341cf4e5962a0ad62e2c0d09a27a4ba43f1
Paul Wilson <ypfrwilson@gmail.com>
2024-05-25 16:44:17 -0400

    move model registry here

--- models/__init__.py
+++ models/__init__.py
@@ -1,2 +1,66 @@
-from .vision_transformer import vit_tiny, vit_small, vit_base, vit_large, vit_small_fft
+from .vision_transformer import VisionTransformer
+from .patch_embed import PatchEmbed, FFTPatchEmbed, FFTPatchEmbedV2
 from .swin_transformer import swin_tiny, swin_small, swin_base, swin_large
+
+
+def vit_tiny(patch_size=16, **kwargs):
+    model = VisionTransformer(
+        patch_size=patch_size,
+        embed_dim=192,
+        depth=12,
+        num_heads=3,
+        mlp_ratio=4,
+        qkv_bias=True,
+        **kwargs
+    )
+    return model
+
+
+def vit_small(patch_size=16, **kwargs):
+    model = VisionTransformer(
+        patch_size=patch_size,
+        embed_dim=384,
+        depth=12,
+        num_heads=6,
+        mlp_ratio=4,
+        qkv_bias=True,
+        **kwargs
+    )
+    return model
+
+
+def vit_base(patch_size=16, **kwargs):
+    model = VisionTransformer(
+        patch_size=patch_size,
+        embed_dim=768,
+        depth=12,
+        num_heads=12,
+        mlp_ratio=4,
+        qkv_bias=True,
+        **kwargs
+    )
+    return model
+
+
+def vit_large(patch_size=16, **kwargs):
+    model = VisionTransformer(
+        patch_size=patch_size,
+        embed_dim=1024,
+        depth=24,
+        num_heads=16,
+        mlp_ratio=4,
+        qkv_bias=True,
+        **kwargs
+    )
+    return model
+
+
+def vit_small_fft_v2(patch_size=16, **kwargs):
+    return vit_small(patch_size=patch_size, patch_embed_cls=FFTPatchEmbedV2, **kwargs)
+
+
+def vit_small_fft_v1(patch_size=16, **kwargs): 
+    return vit_small(patch_size=patch_size, patch_embed_cls=FFTPatchEmbed, **kwargs)  
+
+
+
