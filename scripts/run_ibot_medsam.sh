91d65b4e9169c4da2cd203369f7f8f27d7b940ae
Paul Wilson <ypfrwilson@gmail.com>
2024-05-25 16:44:30 -0400

    new ibot medsam script

---
+++ scripts/run_ibot_medsam.sh
@@ -0,0 +1,42 @@
+echo SLURM_LOCALID:$SLURM_LOCALID
+echo SLURM_PROCID:$SLURM_PROCID
+echo $CUDA_VISIBLE_DEVICES
+
+CHECKPOINT_DIR=/checkpoint/$USER/$SLURM_JOB_ID
+EXPERIMENT_DIR=experiment/$SLURM_JOB_ID
+
+if [ $SLURM_LOCALID = 0 ]
+then
+    if [ ! -d $EXPERIMENT_DIR ] 
+    then
+        ln -s $CHECKPOINT_DIR $EXPERIMENT_DIR
+    fi
+fi
+
+# fix syntax error
+# if [ ! -d $EXPERIMENT_DIR]
+# then
+#     ln -s $CHECKPOINT_DIR $EXPERIMENT_DIR
+# fi
+
+
+# link checkpoint dir to experiments 
+
+export MASTER_PORT=29500
+export MASTER_ADDR=localhost
+export RANK=$SLURM_LOCALID
+export LOCAL_RANK=$SLURM_LOCALID
+export WORLD_SIZE=$SLURM_NTASKS
+
+export WANDB_RUN_ID=$SLURM_JOB_ID
+export WANDB_RESUME='ALLOW'
+export TQDM_DISABLE='True'
+
+IMAGENET_DATA=/scratch/ssd004/datasets/imagenet256/train 
+MICROUS_DATA=/ssd005/projects/exactvu_pca/unlabelled_microus_png
+
+python main_ibot.py -c conf/medsam_ibot.yaml \
+    batch_size_per_gpu=2 \
+    output_dir=$EXPERIMENT_DIR \
+    transform.global_crops_size=512 \
+    epochs=500 \
